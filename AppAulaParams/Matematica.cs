af9b361498786451b9e18189986b16f3e3e27c99
Jhonny <jhonny.god@gmail.com>
2022-03-31 11:37:02 -0300

    Add project files.

---
+++ AppAulaParams/Matematica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AppAulaParams
+{
+    internal class Matematica
+    {
+        public void Multiplicar (int a, int b, out int c)
+        {
+            c = a * b;
+
+        }
+        public void MultiplicarESomar(int a, int b, out int rm, out int rs)
+        {
+            rm = a * b;
+            rs = a + b;
+        }
+        public double Elevar (double n, double pot=2)
+        {
+            return Math.Pow(n, pot);
+
+        }
+    }
+}
---
+++ AppAulaParams/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace AppAulaParams
+{
+    class Program
+    {
+        static void Main()
+        {
+            //passando refencia com classe externa sobre parametros
+
+            Matematica mat = new Matematica();
+
+            int result = 1;
+            mat.Multiplicar(10, 2, out result);
+            Console.WriteLine("Paremetros por valor e referencia" +
+                " Multiplicar informando a saida c OUT \n" + result + "\n");
+
+            // resultados com multiplicaçao e soma na referencia com saida out
+            int a = 10;
+            int b = 2;
+            int resutado;
+            mat.Multiplicar(a, b, out resutado);
+            int r1;
+            int r2;
+            mat.MultiplicarESomar(3, 6, out r1, out r2);
+            Console.WriteLine("Resultado Parametro Multiplicar e Somar \n");
+            Console.WriteLine("A multiplicação de A e B: "+r1 );
+            Console.WriteLine("A soma de A e B: " + r2 + "\n");
+
+            // Parametros Opcionais 
+
+            // double pot = mat.Elevar(2, 3);
+            //parametros opcionais podem ser passados ou não aqui e
+            //quando não passado assume o valor definido como abaixo
+            double pot = mat.Elevar(2);
+            Console.WriteLine("Numero elevado é:" + pot+ "\n");
+
+
+            // classe interna para passar parametros de copia e refernecia
+            MyClass m = new MyClass();
+
+            int i = 5;
+            m.Trocar(ref i);
+            Console.WriteLine("Paremetros por valor e referencia em MyClass");
+            Console.WriteLine(i +"\n");
+
+
+            // segunda classe em parametros
+
+            Numero num = new Numero();
+            num.n = 5;
+            m.Mudar( num);
+            Console.WriteLine("Paremetros por valor e referencia Classe Numero");
+            Console.WriteLine(num.n); 
+        }
+    }
+
+    class MyClass
+    {
+        //passagem por referencia
+        public void Trocar(ref int x)
+        {
+            x = 10;
+        }
+        // passagem por copia
+        public void Mudar( Numero x)
+        {
+            //x.n = 10;
+            x = new Numero();
+            x.n = 20;
+        }
+            }
+    class Numero
+    {
+        public int n;
+
+    }
+}
